ccf321bf4a9a686d2f64d96d0be8f5895cac060f
fal <fal@netcompany.com>
2021-11-05 18:04:43 +0100

    use handtype

--- bots/FalBot.py
+++ bots/FalBot.py
@@ -3,10 +3,14 @@ import random
 from typing import Sequence
 
 from bots.BotInterface import BotInterface
-from environment.Constants import Action
+from environment.Constants import Action, Stage
 from environment.Observation import Observation
 
+from utils.handValue import *
+
 # your bot class, rename to match the file name
+
+
 class FalBot(BotInterface):
 
     # change the name of your bot here
@@ -14,7 +18,7 @@ class FalBot(BotInterface):
         '''init function'''
         super().__init__(name=name)
 
-    def act(self, action_space:Sequence[Action], observation:Observation) -> Action: 
+    def act(self, action_space: Sequence[Action], observation: Observation) -> Action:
         '''
             This function gets called whenever it's your bots turn to act.
                 Parameters:
@@ -24,7 +28,43 @@ class FalBot(BotInterface):
                     action (Action): the action you want you bot to take. Possible actions are: FOLD, CHECK, CALL and RAISE
             If this function takes longer than 1 second, your bot will fold
         '''
+        handType, handTypeCards = getHandType(
+            observation.myHand, observation.boardCards)
 
-        # do a random action
-        action = random.choice(action_space)
-        return action
+        if handType in [HandType.STRAIGHTFLUSH,
+                        HandType.FOUROFAKIND,
+                        HandType.FULLHOUSE,
+                        HandType.FLUSH,
+                        HandType.STRAIGHT,
+                        HandType.THREEOFAKIND,
+                        HandType.TWOPAIR]:
+            return Action.RAISE
+        if observation.stage == Stage.PREFLOP:
+            return self.handlePreFlop(observation)
+
+        return self.handlePostFlop(observation)
+
+    def handlePreFlop(self, observation: Observation) -> Action:
+        # get my hand's percent value (how good is this 2 card hand out of all possible 2 card hands)
+        handPercent, _ = getHandPercent(observation.myHand)
+        # if my hand is top 20 percent: raise
+        if handPercent < .20:
+            return Action.RAISE
+        # if my hand is top 60 percent: call
+        elif handPercent < .60:
+            return Action.CALL
+        # else fold
+        return Action.FOLD
+
+    def handlePostFlop(self, observation: Observation) -> Action:
+        # get my hand's percent value (how good is the best 5 card hand i can make out of all possible 5 card hands)
+        handPercent, cards = getHandPercent(
+            observation.myHand, observation.boardCards)
+        # if my hand is top 30 percent: raise
+        if handPercent <= .30:
+            return Action.RAISE
+        # if my hand is top 80 percent: call
+        elif handPercent <= .80:
+            return Action.CALL
+        # else fold
+        return Action.FOLD
